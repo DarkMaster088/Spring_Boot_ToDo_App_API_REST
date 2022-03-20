900762097e4a712d811a57d2477a4bef5643e2b6
Orelbi Acosta <orelbi088@gmail.com>
2022-03-20 02:50:15 -0400

    Create Task controller implemented.

--- src/main/java/com/oag/todoapp/controller/TaskController.java
+++ src/main/java/com/oag/todoapp/controller/TaskController.java
@@ -1,7 +1,26 @@
 package com.oag.todoapp.controller;
 
+import com.oag.todoapp.dto.TaskInDTO;
+import com.oag.todoapp.persistence.entity.Task;
+import com.oag.todoapp.service.TaskService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO){
+        return this.taskService.createTask(taskInDTO);
+    }
+
 }
